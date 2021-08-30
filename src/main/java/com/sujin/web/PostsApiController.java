7e25fa4af66b3c2046a1d1a610b4c634dcf1795a
sujin418 <wjdtnwls231629@gmail.com>
2021-08-30 10:57:49 +0900

    Posts 수정, 조회 API

--- src/main/java/com/sujin/web/PostsApiController.java
+++ src/main/java/com/sujin/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.sujin.web;
 
 import com.sujin.service.posts.PostsService;
+import com.sujin.web.dto.PostsResponseDto;
 import com.sujin.web.dto.PostsSaveRequestDto;
+import com.sujin.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,4 +18,15 @@ public class PostsApiController {
                                             requestDto) {
         return postsService.save(requestDto);
     }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
 }
